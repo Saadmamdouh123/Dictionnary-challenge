279428c94fd37e161632fa60c07213fca11f4a70
Saadmamdouh123 <saadmamdouh6@gmail.com>
2025-07-03 20:53:02 +0100

    GestTask2

--- GestFichier.py
+++ GestFichier.py
@@ -1,4 +1,4 @@
-# import os
+import os
 
 # folder_path = "C:/Users/Saad/Downloads"
 # liv = os.listdir(folder_path)
@@ -13,20 +13,22 @@
 # print(f.read())
 ######################
 
-data = data2 = ""
+# data = data2 = ""
 
-with open('file.txt') as fp:
-    data = fp.read()
-print(data)
+# with open('file.txt') as fp:
+#     data = fp.read()
+# print(data)
 
-with open('hello.txt') as fp:
-    data2 = fp.read()
+# with open('hello.txt') as fp:
+#     data2 = fp.read()
 
-data += "\n"
-data += data2
+# data += "\n"
+# data += data2
 
-with open ('file2.txt', 'w') as fp:
-    fp.write(data)
+# with open ('file2.txt', 'w') as fp:
+#     fp.write(data)
+
+print(os.path.exists('C:/Users/Saad/Downloads/pythonlearn.pdf'))
 
 
 
