10d66e3b1a342b2cb3b4c765b55a93b808757d69
Saadmamdouh123 <saadmamdouh6@gmail.com>
2025-07-03 16:52:33 +0100

    first OOP task

--- Dic1.py
+++ Dic1.py
@@ -13,23 +13,32 @@ First_dict['RAM'] = "32"
 # d = {'gfg' : 4, 'is' : 2, 'best' : 5}
 # print(str(d))
 
-for n in list(First_dict.keys()):
-    if n == 'Carte Graphique' or n == 'Processeur':
-        # print(n)
-        val = First_dict.pop(n)
-        First_dict[val] = n
-        
-
-print(First_dict)
-add = { "Système d’exploitation": "Windows 10"}
-First_dict.update(add)
-print(First_dict)
-
 # for n in list(First_dict.keys()):
 #     if n == 'Carte Graphique' or n == 'Processeur':
-#         vall1 = reversed[3]
-#         vall2 = reversed[5]
-#         vall1 = vall2
-#     print(list)
+#         # print(n)
+#         val = First_dict.pop(n)
+#         First_dict[val] = n
+# print(First_dict)
+# add = { "Système d’exploitation": "Windows 10"}
+# First_dict.update(add)
+# print(First_dict)
+
+##################
+
+notes_eleves = { "Amine": 15.5, "Yassine": 19.0, "Reda": 14.2, "Malak": 8.7, "Manal": 20.0, "Ahmed": 7.5,"Saad": 11.3, "Hannae": 9.8 }
+dic1 = {}
+dic2 = {}
+for a,b in notes_eleves.items():
+    if b >= 10 :
+        dic1[a] = b
+    else :
+        dic2[a] = b
+print(dic1)
+print(dic2)
+        
+
+
+
+
 
 
---
+++ OOP.py
@@ -0,0 +1,15 @@
+# class CompteBancaire :
+#     def __init__(self,name ,solde = 0):
+#         self.name = name
+#         self.solde = solde
+#     def deposer(self,montant):
+#         self.solde= montant + self.solde
+#     def retirer (self,montant):
+#         if montant < self.solde:
+#             self.solde = self.solde - montant
+#     def afficher(self):
+#         print(self.solde)
+
+###################################
+
+class
